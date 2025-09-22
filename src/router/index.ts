75f861e51e2bbbe64cafebcfb68d853755042d7c
gopinath0332-del <gopinath0332@gmail.com>
2025-09-22 08:00:40 +0000

    Remove unused components, views, and assets to streamline the project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import DashboardView from '../views/DashboardView.vue'
 
 const router = createRouter({
@@ -8,21 +7,13 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: DashboardView,
     },
     {
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+    }
   ],
 })
 
