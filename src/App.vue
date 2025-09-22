dd31979f8a3aad6f14b870049c9ab07bf7473037
gopinath0332-del <gopinath0332@gmail.com>
2025-09-22 07:35:29 +0000

    Remove navigation bar and unused RouterLink imports from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
-      <div class="container">
-        <RouterLink class="navbar-brand" to="/">Expense Logger</RouterLink>
-        
-        <div class="navbar-nav">
-          <RouterLink class="nav-link" to="/">Home</RouterLink>
-          <RouterLink class="nav-link" to="/dashboard">Dashboard</RouterLink>
-          <RouterLink class="nav-link" to="/about">About</RouterLink>
-        </div>
-      </div>
-    </nav>
-
     <RouterView />
   </div>
 </template>
@@ -23,10 +11,5 @@ import { RouterLink, RouterView } from 'vue-router'
 <style>
 #app {
   min-height: 100vh;
-}
-
-.nav-link.router-link-exact-active {
-  color: #dc3545 !important;
-  font-weight: bold;
 }
 </style>
